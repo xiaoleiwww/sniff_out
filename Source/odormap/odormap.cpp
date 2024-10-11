fefa8d69b4ef6a163475745b52282bd3928bcb10
xiaoleiwww <39595279+xiaoleiwww@users.noreply.github.com>
2024-10-11 20:15:32 +0800

    Create odormap.cpp

---
+++ Source/odormap/odormap.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "odormap.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, odormap, "odormap" );
